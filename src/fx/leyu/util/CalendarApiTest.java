f656f1d18cdf5f02de3ea969ea541a10653fb7af
fxleyu <fxleyu@qq.com>
2017-03-26 16:32:35 +0800

    INSERT the code to test JDK Calendar API

---
+++ src/fx/leyu/util/CalendarApiTest.java
@@ -0,0 +1,20 @@
+package fx.leyu.util;
+
+import java.util.Calendar;
+
+public class CalendarApiTest {
+
+	public static void main(String[] args) {
+		instanceTest();
+	}
+
+	public static void instanceTest() {
+		Calendar c = Calendar.getInstance();
+		c.set(Calendar.MONTH, Calendar.FEBRUARY);
+		c.set(Calendar.DAY_OF_MONTH, 32);
+		
+		System.out.println(c.get(Calendar.MONTH));  
+		System.out.println(c.getTime());
+	}
+
+}
