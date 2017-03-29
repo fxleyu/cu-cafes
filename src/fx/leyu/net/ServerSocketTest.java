a52e8ec821cd97d56cbc961df8a82ad58a6b6333
fxleyu <fxleyu@qq.com>
2017-03-29 22:16:14 +0800

    INSERT the code of socket test

---
+++ src/fx/leyu/net/ServerSocketTest.java
@@ -0,0 +1,30 @@
+package fx.leyu.net;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class ServerSocketTest {
+
+    public static void main(String[] args) throws IOException {
+        ServerSocket ss = new ServerSocket(10240);
+        while (true) {
+            Socket socket = ss.accept();
+            new Thread(new Runnable() {
+                public void run() {
+                    try {
+                        OutputStream outputStream = socket.getOutputStream();
+                        outputStream.write("Hi, 我是乐雨！".getBytes("utf-8"));
+                        outputStream.close();
+                        socket.close();
+                    } catch (IOException e) {
+                        // TODO Auto-generated catch block
+                        e.printStackTrace();
+                    }
+                }
+            }).start();
+        }
+    }
+
+}
---
+++ src/fx/leyu/net/SocketTest.java
@@ -0,0 +1,18 @@
+package fx.leyu.net;
+
+import java.io.IOException;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.Scanner;
+
+public class SocketTest {
+
+    public static void main(String[] args) throws UnknownHostException, IOException {
+        Socket socket = new Socket("127.0.0.1", 10240);
+        Scanner scanner = new Scanner(socket.getInputStream());
+        System.out.println(new String(scanner.nextLine().getBytes(), "utf-8"));
+        scanner.close();
+        socket.close();
+    }
+
+}
