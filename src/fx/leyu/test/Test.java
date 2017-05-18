d2ac5f3ee2d0ce04d2b8b70c7e5cbe95f9b0d3c7
fxleyu <fxleyu@qq.com>
2017-05-18 23:39:46 +0800

    DELETE the code for test code

--- src/fx/leyu/test/Test.java
+++ src/fx/leyu/test/Test.java
@@ -2,14 +2,8 @@ package fx.leyu.test;
 
 public class Test {
 
-    public static void main(String[] args) {
-        int i = 1024;
-        short s = 1024;
-        long l = 1024;
-        byte b = 24;
-        
-        double d = 1.1;        
-
+    public static void main(String[] args) {  
+        System.out.println("Hello world!");
     }
 
 }
