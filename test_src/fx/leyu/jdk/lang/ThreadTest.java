b6a82c011c3be76c57e77bc54cd51e370172bc6e
fxleyu <fxleyu@qq.com>
2017-08-29 23:04:34 +0800

    INSERT 添加测试Thread方法的测试用例

---
+++ test_src/fx/leyu/jdk/lang/ThreadTest.java
@@ -0,0 +1,139 @@
+package fx.leyu.jdk.lang;
+
+import org.junit.Test;
+
+public class ThreadTest {
+    
+    @Test
+    public void testJoin() {
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("Thread start");
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+                System.out.println("Thread stop!");
+            }
+        });
+        thread.start();
+        try {
+            System.out.println("Join before");
+            thread.join();
+            System.out.println("Join end");
+        } catch (InterruptedException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+    
+    /*
+     * OUT: 
+     * Join before
+     * Thread start
+     * Join end
+     * Thread stop!
+     */
+    public static void main(String[] args) throws InterruptedException {
+        new ThreadTest().testJoinWithArgs();
+    }
+    
+    
+    /*
+     * OUT: 
+     * Join before
+     * Thread start
+     * Join end
+     */
+    @Test
+    public void testJoinWithArgs() throws InterruptedException {
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("Thread start");
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    System.out.println(e);
+                    e.printStackTrace();
+                } catch(Exception e) {
+                    System.out.println(e);
+                    e.printStackTrace();
+                }
+                System.out.println("Thread stop!");
+            }
+        });
+        thread.start();
+        try {
+            System.out.println("Join before");
+            thread.join(500);
+            System.out.println("Join end");
+        } catch (InterruptedException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void testStop() {
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("Thread start");
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    System.out.println(e);
+                    e.printStackTrace();
+                } catch(Exception e) {
+                    System.out.println(e);
+                    e.printStackTrace();
+                }
+                System.out.println("Thread stop!");
+            }
+        });
+        thread.start();
+        try {
+            System.out.println("Join before");
+            thread.join(500);
+            System.out.println("Join end");
+        } catch (InterruptedException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        thread.stop();
+    }
+    
+    @Test
+    public void testInterrupt() {
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("Thread start");
+                try {
+                    while(true) {
+                        System.out.println("test .... ");
+                        
+                    }
+                } catch(Exception e) {
+                    System.out.println(e);
+                    e.printStackTrace();
+                }
+                System.out.println("Thread stop!");
+            }
+        });
+        thread.start();
+        try {
+            System.out.println("Join before");
+            thread.join(500);
+            System.out.println("Join end");
+        } catch (InterruptedException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        thread.interrupt();
+    }
+}
