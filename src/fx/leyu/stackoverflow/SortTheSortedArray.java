8369680dbea8f5a1c06a217340a0fcf959d1377e
fxleyu <fxleyu@qq.com>
2017-11-06 23:25:09 +0800

    UPDATE 练习代码

---
+++ src/fx/leyu/stackoverflow/SortTheSortedArray.java
@@ -0,0 +1,29 @@
+package fx.leyu.stackoverflow;
+
+import java.util.Arrays;
+import java.util.Random;
+
+public class SortTheSortedArray {
+
+    public static void main(String[] args) {
+        // Generate data
+        int arraySize = 32768;
+        int data[] = new int[arraySize];
+
+        Random rnd = new Random(0);
+        for (int c = 0; c < arraySize; ++c) {
+            data[c] = rnd.nextInt(arraySize);
+        }
+        
+        long start = System.nanoTime();
+        // !!! With this, the next loop runs faster
+        Arrays.sort(data);
+        System.out.println("1 : " + (System.nanoTime() - start));
+
+        start = System.nanoTime();
+        Arrays.sort(data);
+        System.out.println("2 : " + (System.nanoTime() - start));
+
+    }
+
+}
