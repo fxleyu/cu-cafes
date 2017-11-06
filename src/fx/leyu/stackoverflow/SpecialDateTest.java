0b1eef4052bbe6808b2e4f1cdc91721f933d8e19
fxleyu <fxleyu@qq.com>
2017-11-06 23:27:39 +0800

    UPDATE 优化代码

--- src/fx/leyu/stackoverflow/SpecialDateTest.java
+++ src/fx/leyu/stackoverflow/SpecialDateTest.java
@@ -8,8 +8,6 @@ import java.util.TimeZone;
 public class SpecialDateTest {
 
     public static void main(String[] args) throws ParseException {
-        System.out.println(TimeZone.getDefault());
-        System.out.println(TimeZone.getDefault().useDaylightTime());
         SimpleDateFormat sf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");  
         String str3 = "1927-12-31 23:54:07";  
         String str4 = "1927-12-31 23:54:08";  
@@ -20,15 +18,6 @@ public class SpecialDateTest {
         System.out.println(ld4-ld3);
         
         System.out.println(TimeZone.getDefault());
-        
-        long startOf1900Utc = -2208988800000L;
-        for (String id : TimeZone.getAvailableIDs()) {
-            TimeZone zone = TimeZone.getTimeZone(id);
-            if (zone.getRawOffset() != zone.getOffset(startOf1900Utc - 1)) {
-                System.out.println(id);
-            }
-            System.out.println(id);
-        }
     }
 
 }
