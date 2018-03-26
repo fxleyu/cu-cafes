c9115b33e0a51b269558d01e4626bba70d48a76e
fxleyu <fxleyu@qq.com>
2018-03-26 23:37:04 +0800

    FIXED #31 添加 String 测试代码

---
+++ jdk-test/src/test/java/fx/leyu/jdk/lang/StringTest.java
@@ -0,0 +1,42 @@
+package fx.leyu.jdk.lang;
+
+import org.apache.commons.lang3.StringUtils;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+public class StringTest {
+
+    @Test
+    public void testEquals() {
+        String one = "Hello";
+        String other = new String(one);
+        Assert.assertTrue(one.equals(other));
+
+        Assert.assertFalse(one == other);
+
+        Assert.assertTrue(one == other.intern());
+    }
+
+    @Test
+    public void testRepeatString() {
+        String str = "123";
+        String str3 = "123123123";
+        Assert.assertEquals(str3, StringUtils.repeat(str, 3));
+    }
+
+    @Test
+    public void testStringDate() {
+        String str = "Sep 17, 2013";
+        try {
+            Date date = new SimpleDateFormat("MMMM d, yy", Locale.ENGLISH).parse(str);
+            Assert.assertEquals(8, date.getMonth());
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+    }
+}
--- read-blog/README.md
+++ read-blog/README.md
@@ -5,5 +5,10 @@
 - [ ] 避免Java应用中NullPointerException的技巧和最佳实践
 
 
+## Java 基础
+- [x] [10个有关String的面试问题](https://github.com/fxleyu/cu-cafes/issues/31)
+
 ## Java Web相关
-- [ ] [Web服务器的工作原理](https://github.com/fxleyu/cu-cafes/issues/30)
+- [x] [Web服务器的工作原理](https://github.com/fxleyu/cu-cafes/issues/30)
+
+
