7204a0842675d3dabd9793a3eb13cf24e9ba285d
fxleyu <fxleyu@qq.com>
2018-04-02 23:46:31 +0800

    INSERT 学习 Object 类的方法

---
+++ jdk-test/src/test/java/fx/leyu/jdk/lang/ObjectTest.java
@@ -0,0 +1,52 @@
+package fx.leyu.jdk.lang;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ObjectTest {
+    @Test
+    public void testGetClass() {
+        Object obj = new Object();
+        Assert.assertEquals(Object.class, obj.getClass());
+    }
+
+    @Test
+    public void testEquals() {
+        Object obj = new Object();
+        Assert.assertFalse(obj.equals(null));
+    }
+
+    @Test
+    public void testHashCode() {
+        Object obj = new Object();
+        Assert.assertEquals(369241501, obj.hashCode());
+    }
+
+    @Test
+    public void testToString() {
+        Object obj = new Object();
+        Assert.assertEquals("java.lang.Object@281e3708", obj.toString());
+    }
+
+    @Test
+    public void testWaiting() {
+        Object obj = new Object();
+        synchronized (obj) {
+            try {
+                obj.wait(222);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        Assert.assertEquals(true, true);
+    }
+
+    @Test
+    public void testNotify() {
+        final Object obj = new Object();
+        synchronized (obj) {
+            obj.notify();
+        }
+        Assert.assertEquals(null, null);
+    }
+}
