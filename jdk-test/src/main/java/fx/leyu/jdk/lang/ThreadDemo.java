99cf13fc7682f2b714852fa0a1ee9f27cbced699
fxleyu <fxleyu@qq.com>
2018-04-28 21:57:07 +0800

    [学习] 学习测试线程中断

--- jdk-test/src/main/java/fx/leyu/jdk/lang/ThreadDemo.java
+++ jdk-test/src/main/java/fx/leyu/jdk/lang/ThreadDemo.java
@@ -1,5 +1,7 @@
 package fx.leyu.jdk.lang;
 
+import org.apache.commons.lang3.RandomUtils;
+
 public class ThreadDemo {
 
     public static void handleRunnable() {
@@ -13,8 +15,29 @@ public class ThreadDemo {
         }
     }
 
-    public static void main(String[] args) {
-        handleRunnable();
-        handleThreadState();
+    public static void main(String[] args) throws InterruptedException {
+        Runnable a = () -> {
+                while (true) {
+                    int x = RandomUtils.nextInt(0, 100000000);
+                    if (x % 100 == 0) {
+                        if (x == 6000) {
+                            try {
+                                System.out.println("a rr sleep before");
+                                Thread.sleep(100);
+                            } catch (InterruptedException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    }
+                }
+        };
+
+        Thread.currentThread().interrupt();
+        Thread t = new Thread(a);
+        t.start();
+        System.out.println("sleep before");
+        Thread.sleep(50);
+        System.out.println("sleep end");
+        t.interrupt();
     }
 }
---
+++ jdk-test/src/test/java/fx/leyu/jdk/lang/ThreadDemoTest.java
@@ -0,0 +1,16 @@
+package fx.leyu.jdk.lang;
+
+import org.junit.Test;
+
+public class ThreadDemoTest {
+
+    @Test
+    public void testHandleRunnable() {
+        ThreadDemo.handleRunnable();
+    }
+
+    @Test
+    public void testHandleThreadState() {
+        ThreadDemo.handleThreadState();
+    }
+}
