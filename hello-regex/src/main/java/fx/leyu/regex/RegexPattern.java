6cbe675b4afad1c2dc0f46c88c06ee8d4afb5252
fxleyu <fxleyu@qq.com>
2018-06-24 12:52:02 +0800

    [REGEX] 练习匹配一组字符

--- hello-regex/src/main/java/fx/leyu/regex/RegexPattern.java
+++ hello-regex/src/main/java/fx/leyu/regex/RegexPattern.java
@@ -1,5 +1,9 @@
 package fx.leyu.regex;
 
 public class RegexPattern {
-    public static final String ESCAPE_CHARACTER = "\\";
+    public static final String ESCAPE_CHARACTER = "\\";             // 转义字符
+    public static final String MONTH_PATTERN = "(1[0-2])|[1-9]";   // 月份
+    public static final String DOT_CHARACTER = ".";                 // 任意字符
+    public static final String NET_NAME_PATTERN = "[Ff]x[Ll]eyu";  // 区间匹配
+    public static final String NOT_NUMBER_PATTERN = "[^0-9]";       // 非数字
 }
--- hello-regex/src/test/java/fx/leyu/regex/RegexPatternTest.java
+++ hello-regex/src/test/java/fx/leyu/regex/RegexPatternTest.java
@@ -6,7 +6,42 @@ import org.junit.Test;
 public class RegexPatternTest {
     @Test
     public void testMetacharacter() {
-        String test = "\\";
         Assert.assertTrue("file.txt".matches("file" + RegexPattern.ESCAPE_CHARACTER + ".txt"));
+    }
+
+    @Test
+    public void testNetName() {
+        Assert.assertTrue("fxleyu".matches(RegexPattern.NET_NAME_PATTERN));
+        Assert.assertTrue("Fxleyu".matches(RegexPattern.NET_NAME_PATTERN));
+        Assert.assertTrue("FxLeyu".matches(RegexPattern.NET_NAME_PATTERN));
+        Assert.assertFalse("FXleyu".matches(RegexPattern.NET_NAME_PATTERN));
+    }
+
+    @Test
+    public void testDotCharacter() {
+        Assert.assertTrue("fxleyu".matches("fxley" + RegexPattern.DOT_CHARACTER));
+        Assert.assertTrue("fxleyx".matches("fxley" + RegexPattern.DOT_CHARACTER));
+    }
+
+    @Test
+    public void testMonth() {
+        Assert.assertTrue("1".matches(RegexPattern.MONTH_PATTERN));
+        Assert.assertTrue("9".matches(RegexPattern.MONTH_PATTERN));
+        Assert.assertTrue("10".matches(RegexPattern.MONTH_PATTERN));
+        Assert.assertTrue("12".matches(RegexPattern.MONTH_PATTERN));
+
+        Assert.assertFalse("0".matches(RegexPattern.MONTH_PATTERN));
+        Assert.assertFalse("13".matches(RegexPattern.MONTH_PATTERN));
+        Assert.assertFalse("213".matches(RegexPattern.MONTH_PATTERN));
+    }
+
+    @Test
+    public void testNotNumber() {
+        Assert.assertFalse("0".matches(RegexPattern.NOT_NUMBER_PATTERN));
+        Assert.assertFalse("1".matches(RegexPattern.NOT_NUMBER_PATTERN));
+        Assert.assertFalse("9".matches(RegexPattern.NOT_NUMBER_PATTERN));
+
+        Assert.assertTrue("F".matches(RegexPattern.NOT_NUMBER_PATTERN));
+        Assert.assertTrue("朱".matches(RegexPattern.NOT_NUMBER_PATTERN));
     }
 }
