46deca93ee1c771577657e2d39fcd6e7e7d1d053
fxleyu <fxleyu@qq.com>
2018-07-14 23:57:12 +0800

    [学习][Redis] 添加 Java 访问 Redis 的 Hello world!

--- hello-redis/src/main/java/fx/leyu/redis/string/HelloString.java
+++ hello-redis/src/main/java/fx/leyu/redis/string/HelloString.java
@@ -1,4 +1,14 @@
 package fx.leyu.redis.string;
 
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPoolConfig;
+
 public class HelloString {
+    public static void main(String[] args) {
+        JedisPool pool = new JedisPool(new JedisPoolConfig(), "192.168.199.120");
+        try (Jedis jedis = pool.getResource()) {
+            jedis.set("hello", "world");
+        }
+    }
 }
