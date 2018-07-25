356476ec6adf652b5bcc279d95e922b4b1be92ca
fxleyu <fxleyu@qq.com>
2018-07-25 22:32:07 +0800

    [学习][Redis] 添加 Java 访问 Redis 的第一个存入存储的操作及耗时

---
+++ hello-redis/src/test/java/fx/leyu/redis/string/HelloStringTest.java
@@ -0,0 +1,30 @@
+package fx.leyu.redis.string;
+
+import org.junit.Assert;
+import org.junit.Test;
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPoolConfig;
+
+public class HelloStringTest {
+    @Test
+    public void testSetAndGet() {
+        String key = "hello";
+        String value = "world";
+        try(JedisPool pool = new JedisPool(new JedisPoolConfig(), "192.168.199.120")) {
+            try (Jedis jedis = pool.getResource()) {
+                long start = System.nanoTime();
+                jedis.set(key, value);
+                long end = System.nanoTime();
+                System.out.println("Redis String set " + (end - start) / 1000000);
+            }
+            try (Jedis jedis = pool.getResource()) {
+                long start = System.nanoTime();
+                String actualValue = jedis.get(key);
+                long end = System.nanoTime();
+                System.out.println("Redis String get " + (end - start) / 1000000);
+                Assert.assertEquals(value, actualValue);
+            }
+        }
+    }
+}
