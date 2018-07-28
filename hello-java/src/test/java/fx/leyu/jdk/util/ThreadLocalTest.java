a7dbacb9d906a4a9e79377e8dc6256ff9fe1a992
fxleyu <fxleyu@qq.com>
2018-07-28 21:41:05 +0800

    [TEST] 处理线程本地测试，失败
    Signed-off-by: fxleyu <fxleyu@qq.com>

---
+++ hello-java/src/test/java/fx/leyu/jdk/util/ThreadLocalTest.java
@@ -0,0 +1,39 @@
+package fx.leyu.jdk.util;
+
+import com.google.common.collect.Maps;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
+public class ThreadLocalTest {
+    private static final String KEY = "key";
+    private static final String VALUE = "value";
+
+    @Test
+    public void test() throws InterruptedException {
+        ThreadLocal<Map<String, String>> threadLocal = new ThreadLocal<>();
+        threadLocal.set(Maps.newHashMap());
+        threadLocal.get().put(KEY, VALUE);
+
+        new Thread(() -> {
+            try {
+                TimeUnit.SECONDS.sleep(2);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            Assert.assertEquals(VALUE, threadLocal.get().get(KEY));
+        }).start();
+
+        new Thread(() -> {
+            try {
+                TimeUnit.SECONDS.sleep(3);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            Assert.assertEquals(VALUE, threadLocal.get().get(KEY));
+        }).start();
+        TimeUnit.SECONDS.sleep(5);
+    }
+}
