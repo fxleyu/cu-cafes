2a0733bdeea30d6d0d9b4a57a1908c45ae1b25ed
fxleyu <fxleyu@qq.com>
2018-07-28 21:40:07 +0800

    [TEST][JVM] 测试 JVM 栈溢出（更新，打印 GC）
    Signed-off-by: fxleyu <fxleyu@qq.com>

--- hello-jvm/src/test/java/fx/leyu/jvm/oom/StackOOMTest.java
+++ hello-jvm/src/test/java/fx/leyu/jvm/oom/StackOOMTest.java
@@ -1,16 +1,26 @@
 package fx.leyu.jvm.oom;
 
+import org.apache.commons.lang3.RandomUtils;
 import org.junit.Test;
+
+import java.util.concurrent.TimeUnit;
 
 public class StackOOMTest {
 
     @Test
-    public void testStackLeakByThread() {
+    public void testStackLeakByThread() throws InterruptedException {
         while (true) {
             new Thread(() -> {
                 while (true) {
+                    RandomUtils.nextInt();
+                    try {
+                        TimeUnit.MILLISECONDS.sleep(20);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
                 }
             }).start();
+            TimeUnit.MILLISECONDS.sleep(1000);
         }
     }
 }
