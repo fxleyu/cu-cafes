32a9cc88ae8f9447786f7447b556095ba0ad1704
朱智慧 <fxleyu@qq.com>
2018-07-30 17:48:13 +0800

    [单元测试] 处理正则中出现一次或者不出现的情况
    Signed-off-by: zhuzhihui <zhuzhihui@jd.com>

---
+++ hello-regex/src/test/java/fx/leyu/regex/business/UrlTest.java
@@ -0,0 +1,18 @@
+package fx.leyu.regex.business;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class UrlTest {
+
+    @Test
+    public void testRemoveScheme() {
+        String httpUrl = "http://en.wikipedia.org/wiki/URL";
+        String httpsUrl = "https://en.wikipedia.org/wiki/URL";
+        String urlWithoutScheme = "//en.wikipedia.org/wiki/URL";
+
+        final String regex = "https?:";
+        Assert.assertEquals(urlWithoutScheme, httpUrl.replaceAll(regex, ""));
+        Assert.assertEquals(urlWithoutScheme, httpsUrl.replaceAll(regex, ""));
+    }
+}
