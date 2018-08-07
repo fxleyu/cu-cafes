21be4639e88b1bac404ef2b48f130cee042a4d61
朱智慧 <fxleyu@qq.com>
2018-08-07 17:13:19 +0800

    [测试代码][EJ] 泛型测试
    Signed-off-by: zhuzhihui <zhuzhihui@jd.com>

--- hello-java/src/test/java/fx/leyu/jdk/generic/GenericTest.java
+++ hello-java/src/test/java/fx/leyu/jdk/generic/GenericTest.java
@@ -1,6 +1,7 @@
 package fx.leyu.jdk.generic;
 
 import com.google.common.collect.Lists;
+import org.junit.Assert;
 import org.junit.Test;
 
 import javax.annotation.Nonnull;
@@ -23,6 +24,18 @@ public class GenericTest {
         ArrayList<String> stringList = Lists.newArrayList();
         unsafeAdd(stringList, new Integer(2));
         String s = stringList.get(0);
+    }
+
+    @Test(expected = ArrayStoreException.class)
+    public void testArrayCovariant() {
+        Object[] objects = new Long[3];
+        objects[0] = "I don't fit in";
+    }
+
+    @Test
+    public void testListInvariant() {
+        // List<Object> objectList = new ArrayList<Long>(); // compile ERROR
+        Assert.assertTrue(true);
     }
 
     private void unsafeAdd(@Nonnull List<?> stringList, @Nonnull Object o) {
