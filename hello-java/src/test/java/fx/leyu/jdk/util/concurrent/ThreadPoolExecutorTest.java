253bc0fe16244e93d2798cd26255b125fa7e2e1a
zhuzhihui <zhuzhihui@jd.com>
2018-08-08 10:04:22 +0800

    [测试代码][EJ] 泛型测试
    Signed-off-by: zhuzhihui <zhuzhihui@jd.com>

---
+++ hello-java/src/test/java/fx/leyu/jdk/util/concurrent/ThreadPoolExecutorTest.java
@@ -0,0 +1,20 @@
+package fx.leyu.jdk.util.concurrent;
+
+import org.junit.Test;
+
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+public class ThreadPoolExecutorTest {
+
+    @Test
+    public void test() {
+        ThreadPoolExecutor executor = new ThreadPoolExecutor(20, 20, 3, TimeUnit.SECONDS, null);
+        executor.execute(new Runnable() {
+            @Override
+            public void run() {
+
+            }
+        });
+    }
+}
