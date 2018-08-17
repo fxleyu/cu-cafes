438a6d084454fcb2469ebe6c5b6123c43580698c
zhuzhihui <zhuzhihui@jd.com>
2018-08-17 16:36:21 +0800

    [代码优化] 修改不合理的方法命名

--- hello-regex/src/test/java/fx/leyu/regex/LookAroundTest.java
+++ hello-regex/src/test/java/fx/leyu/regex/LookAroundTest.java
@@ -6,32 +6,29 @@ import org.junit.Test;
 import java.util.regex.Pattern;
 
 public class LookAroundTest {
-
     @Test
     public void testPositiveLookahead() {
-        Assert.assertTrue(canMatch("(?<=Timeout)Exception", "TimeoutException"));
-        Assert.assertFalse(canMatch("(?<=Timeout)Exception", "NullPointerException"));
-    }
-
-    @Test
-    public void testNegativeLookahead() {
-        Assert.assertTrue(canMatch("(?<!Timeout)Exception", "NullPointerException"));
-        Assert.assertFalse(canMatch("(?<!Timeout)Exception", "TimeoutException"));
-    }
-
-    @Test
-    public void testPositiveLookbehind () {
         Assert.assertTrue(canMatch("Timeout(?=Exception)", "TimeoutException"));
         Assert.assertFalse(canMatch("Timeout(?=Exception)", "Timeout exception"));
     }
 
     @Test
-    public void testNegativeLookbehind () {
+    public void testNegativeLookahead() {
         Assert.assertTrue(canMatch("Timeout(?!Exception)", "Timeout exception"));
         Assert.assertFalse(canMatch("Timeout(?!Exception)", "TimeoutException"));
     }
 
+    @Test
+    public void testPositiveLookbehind() {
+        Assert.assertTrue(canMatch("(?<=Timeout)Exception", "TimeoutException"));
+        Assert.assertFalse(canMatch("(?<=Timeout)Exception", "NullPointerException"));
+    }
 
+    @Test
+    public void testNegativeLookbehind() {
+        Assert.assertTrue(canMatch("(?<!Timeout)Exception", "NullPointerException"));
+        Assert.assertFalse(canMatch("(?<!Timeout)Exception", "TimeoutException"));
+    }
 
     private static boolean canMatch(String patternString, String string) {
         return Pattern.compile(patternString).matcher(string).find();
