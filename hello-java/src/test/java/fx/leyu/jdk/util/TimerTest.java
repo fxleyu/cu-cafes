a75ddcdcfe6740a62f99f49fcf2bbae035f87ab3
朱智慧 <fxleyu@qq.com>
2018-08-30 23:06:10 +0800

    [TEST] 处理 Timer
    Signed-off-by: fxleyu <fxleyu@qq.com>

---
+++ hello-java/src/test/java/fx/leyu/jdk/util/TimerTest.java
@@ -0,0 +1,29 @@
+package fx.leyu.jdk.util;
+
+import org.junit.Test;
+
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.concurrent.TimeUnit;
+
+public class TimerTest {
+
+    @Test
+    public void testSchedule() throws InterruptedException {
+        Timer timer = new Timer("Schedule print");
+        System.out.println("outer = " + System.currentTimeMillis() / 1000);
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                System.out.println("start " + System.currentTimeMillis() / 1000);
+                try {
+                    TimeUnit.SECONDS.sleep(1);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                System.out.println("end " + System.currentTimeMillis() / 1000);
+            }
+        }, 1000L, 2000L);
+        TimeUnit.SECONDS.sleep(20);
+    }
+}
