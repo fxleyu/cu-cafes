a2aa55897583d8394bd73f302ea148f9e93cfa3e
zhuzhihui <zhuzhihui@jd.com>
2018-09-15 10:09:21 +0800

    [设计模式] 使用 Spring 测试责任链模式（删除不必要的内容）

--- hello-design-pattern/src/main/java/fx/leyu/designpattern/chainofresponsibility/handler/SupermarketMarkingHandler.java
+++ hello-design-pattern/src/main/java/fx/leyu/designpattern/chainofresponsibility/handler/SupermarketMarkingHandler.java
@@ -3,6 +3,8 @@ package fx.leyu.designpattern.chainofresponsibility.handler;
 import fx.leyu.designpattern.chainofresponsibility.MarkingHandler;
 
 public class SupermarketMarkingHandler extends MarkingHandler {
+    protected String word = "超市开始";
+
     @Override
     protected boolean marking() {
         System.out.println("超市");
