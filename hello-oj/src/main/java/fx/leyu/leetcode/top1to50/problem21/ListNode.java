bca72428ccf5ceab97eb5ee7c7adc36bff6b4254
fxleyu <fxleyu@qq.com>
2018-10-12 23:55:09 +0800

    [LeetCode][简单] 21 合并两个有序链表

---
+++ hello-oj/src/main/java/fx/leyu/leetcode/top1to50/problem21/ListNode.java
@@ -0,0 +1,7 @@
+package fx.leyu.leetcode.top1to50.problem21;
+
+public class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) { val = x; }
+}
---
+++ hello-oj/src/main/java/fx/leyu/leetcode/top1to50/problem21/Solution.java
@@ -0,0 +1,24 @@
+package fx.leyu.leetcode.top1to50.problem21;
+
+public class Solution {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if (l1 == null || l2 == null) {
+            return l1 == null ? l2 : l1;
+        }
+        ListNode result = new ListNode(0);
+        ListNode now = result;
+        while (l1 != null && l2 != null) {
+            if (l1.val > l2.val) {
+                now.next = l2;
+                l2 = l2.next;
+            } else {
+                now.next = l1;
+                l1 = l1.next;
+            }
+            now = now.next;
+        }
+
+        now.next = l1 == null ? l2 : l1;
+        return result.next;
+    }
+}
