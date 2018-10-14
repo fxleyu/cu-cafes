318d88dc54f6f6cc0a71b678d9f9133f68af1b25
fxleyu <fxleyu@qq.com>
2018-10-14 23:50:52 +0800

    [LeetCode][简单] 35 搜索插入位置(错误两次：1、二分查找注意一个元素情况； 2、二分查找注意两个元素的情况，可能出现 -1 越界)

---
+++ hello-oj/src/main/java/fx/leyu/leetcode/top1to50/problem35/Solution.java
@@ -0,0 +1,24 @@
+package fx.leyu.leetcode.top1to50.problem35;
+
+public class Solution {
+    public int searchInsert(int[] nums, int target) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        int max = nums.length - 1;
+        int min = 0;
+        while (max > min) {
+            int index = (max + min) / 2;
+            int value = nums[index];
+            if (value > target) {
+                max = index - 1;
+            } else if (value < target) {
+                min = index + 1;
+            } else {
+                return index;
+            }
+        }
+
+        return nums[min] >= target ? min : min + 1;
+    }
+}
