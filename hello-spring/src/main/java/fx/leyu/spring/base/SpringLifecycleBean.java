2e650f8273fe66b994566607036eaed011d1f1da
朱智慧 <fxleyu@qq.com>
2018-11-23 18:10:49 +0800

    [Spring] Bean 初始化模拟（添加自定义初始化方法）

--- hello-spring/src/main/java/fx/leyu/spring/base/SpringLifecycleBean.java
+++ hello-spring/src/main/java/fx/leyu/spring/base/SpringLifecycleBean.java
@@ -3,50 +3,66 @@ package fx.leyu.spring.base;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.*;
 import org.springframework.beans.factory.config.BeanPostProcessor;
-import org.springframework.beans.factory.config.InstantiationAwareBeanPostProcessor;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
-import org.springframework.context.annotation.Bean;
-import org.springframework.lang.Nullable;
-import org.springframework.stereotype.Component;
 
-@Component
 public class SpringLifecycleBean implements BeanNameAware, BeanFactoryAware, ApplicationContextAware, BeanPostProcessor,
         InitializingBean, DisposableBean {
+
+    public SpringLifecycleBean() {
+        System.out.println("INIT");
+    }
+
     @Override
     public void setBeanName(String name) {
         System.out.println("[Lifecycle][BeanNameAware] setBeanName + " + name);
+        //new RuntimeException("setBeanName").printStackTrace();
     }
 
     @Override
     public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
         System.out.println("[Lifecycle][BeanFactoryAware] setBeanFactory + " + beanFactory);
+        //new RuntimeException("setBeanFactory").printStackTrace();
     }
 
     @Override
     public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
         System.out.println("[Lifecycle][ApplicationContextAware] setApplicationContext + " + applicationContext);
+        //new RuntimeException("setApplicationContext").printStackTrace();
     }
 
     @Override
     public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
         System.out.println("[Lifecycle][BeanPostProcessor] postProcessBeforeInitialization + " + beanName);
+        //new RuntimeException("postProcessBeforeInitialization").printStackTrace();
         return bean;
     }
 
     @Override
     public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
-        System.out.println("[Lifecycle][BeanPostProcessor] postProcessAfterInitialization + " + beanName);
+        System.out.println("[Lifecycle][BeanPostProcessor] postProcessAfterInitialization + " + beanName + ", bean class is " + bean.getClass().getName());
+        //new RuntimeException("postProcessAfterInitialization").printStackTrace();
+        System.out.println(this.getClass().getName());
         return bean;
     }
 
     @Override
     public void afterPropertiesSet() throws Exception {
         System.out.println("[Lifecycle][InitializingBean] afterPropertiesSet");
+        //new RuntimeException("afterPropertiesSet").printStackTrace();
     }
 
     @Override
     public void destroy() throws Exception {
         System.out.println("[Lifecycle][DisposableBean] destroy");
+        //new RuntimeException("destroy").printStackTrace();
+    }
+
+    public void initMethod() {
+        System.out.println("[Lifecycle][init-method] initMethod");
+    }
+
+    public void destroyMethod() {
+        System.out.println("[Lifecycle][destroy-method] destroyMethod");
     }
 }
