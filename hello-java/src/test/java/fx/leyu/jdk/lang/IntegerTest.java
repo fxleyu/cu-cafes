885ecc30f7431f9e6722ead3b10b21520fd5c902
fxleyu <fxleyu@qq.com>
2019-01-14 23:46:20 +0800

    [TEST] 二进制转换为 int（32 个 1 不可以转换为 int 的最小值）

--- hello-java/src/test/java/fx/leyu/jdk/lang/IntegerTest.java
+++ hello-java/src/test/java/fx/leyu/jdk/lang/IntegerTest.java
@@ -9,4 +9,17 @@ public class IntegerTest {
     public void testMinValue() {
         Assert.assertTrue(Integer.MIN_VALUE == -Integer.MIN_VALUE);
     }
+
+    @Test
+    public void testBitOperation() {
+        String string = Integer.toString(Integer.MAX_VALUE, 2);
+        int value = Integer.valueOf("-" + string, 2);
+        Assert.assertTrue(value - 1 == Integer.MIN_VALUE);
+    }
+
+    @Test(expected = NumberFormatException.class)
+    public void test32One() {
+        String string = Integer.toString(Integer.MAX_VALUE, 2); // 31 个 1
+        int value = Integer.valueOf("1" + string, 2);
+    }
 }
