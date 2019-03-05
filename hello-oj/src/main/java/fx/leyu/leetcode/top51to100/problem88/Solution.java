b4b1887f71d522fe49cbb54fa41f746bd31ec532
fxleyu <fxleyu@qq.com>
2019-03-05 23:01:00 +0800

    [LeetCode] 88 合并两个有序数组

---
+++ hello-oj/src/main/java/fx/leyu/leetcode/top51to100/problem88/Solution.java
@@ -0,0 +1,19 @@
+package fx.leyu.leetcode.top51to100.problem88;
+
+public class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        if (nums1 == null || m < 0 || n < 0 || nums1.length < m + n) {
+            throw new IllegalArgumentException("ERROR input");
+        }
+
+        int resultIndex = m + n;
+        while (m > 0 && n > 0) {
+            nums1[--resultIndex] = nums1[m - 1] > nums2[n - 1] ? nums1[--m] : nums2[--n];
+        }
+        if (n > 0) {
+            while (n > 0) {
+                nums1[--resultIndex] = nums2[--n];
+            }
+        }
+    }
+}
