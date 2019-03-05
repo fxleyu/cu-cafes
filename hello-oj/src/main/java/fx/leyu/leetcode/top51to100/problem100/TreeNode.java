51e2701400417cebc4a797785ba2a48e33cbc70f
fxleyu <fxleyu@qq.com>
2019-03-05 23:12:16 +0800

    [LeetCode] 100 相同的树

---
+++ hello-oj/src/main/java/fx/leyu/leetcode/top51to100/problem100/TreeNode.java
@@ -0,0 +1,11 @@
+package fx.leyu.leetcode.top51to100.problem100;
+
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+}
