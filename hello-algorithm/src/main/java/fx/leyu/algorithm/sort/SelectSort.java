063eb7e273ca0c5ea27223db126f4f589eb1e431
fxleyu <fxleyu@qq.com>
2019-08-04 18:53:15 +0800

    [算法] 选择排序 重构

--- hello-algorithm/src/main/java/fx/leyu/algorithm/sort/SelectSort.java
+++ hello-algorithm/src/main/java/fx/leyu/algorithm/sort/SelectSort.java
@@ -1,56 +1,39 @@
 package fx.leyu.algorithm.sort;
 
+import java.util.Arrays;
+
 public class SelectSort {
+    public static void main(String[] args) {
+        int data[] = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 5};
+        selectSort(data);
+        out(data);
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
-	//	int data[] = {1,2,3,4,5,6,7,8,9,10};
-		
-		int data[] = {10,9,8,7,6,5,4,3,2,1,5};
-		
-		selectSort(data);
-		
-		out(data);
+    }
 
-	}
-	
-	private static void selectSort(int[] data) {
-		// TODO Auto-generated method stub
-		
-		for(int i=0;i<data.length-1;i++)
-		{
-			int minIndex = i;
-			
-			for(int j=i+1;j<data.length;j++)
-			{
-				if(data[minIndex]>data[j])
-				{
-					minIndex = j;
-					
-				}
-			}
-			if(minIndex != i)
-			{
-				int temp = data[i];
-				data[i] = data[minIndex];
-				data[minIndex] = temp;
-			}
-		}
-		
-	}
+    private static void selectSort(int[] data) {
+        for (int i = 0; i < data.length - 1; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < data.length; j++) {
+                if (data[minIndex] > data[j]) {
+                    minIndex = j;
 
-	private static void out(int[] data) {
-		// TODO Auto-generated method stub
-		for(int i =0;i<data.length;i++)
-		{
-			System.out.print(data[i]+"   ");
-		}
-		System.out.println("");
-		
-	}
+                }
+            }
+            if (minIndex != i) {
+                exchange(data, minIndex, i);
+            }
+        }
+
+    }
+
+    private static void exchange(int[] data, int minIndex, int i) {
+        int temp = data[i];
+        data[i] = data[minIndex];
+        data[minIndex] = temp;
+    }
+
+    private static void out(int[] data) {
+        System.out.println(Arrays.toString(data));
+    }
 
 }
