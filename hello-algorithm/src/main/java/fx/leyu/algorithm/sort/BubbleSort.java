b0548a4bf80e338a50f3cc99598e62db8a2be779
fxleyu <fxleyu@qq.com>
2019-08-04 19:50:05 +0800

    [算法] 冒泡排序原始版（来自于同学，一直存在印象笔记中）

---
+++ hello-algorithm/src/main/java/fx/leyu/algorithm/sort/BubbleSort.java
@@ -0,0 +1,54 @@
+package fx.leyu.algorithm.sort;
+
+public class BubbleSort {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	//	int data[] = {1,2,3,4,5,6,7,8,9,10};
+		
+		int data[] = {10,9,8,7,6,5,4,3,2,1};
+		
+		bubbleSort(data);
+		
+		out(data);
+
+	}
+	
+	private static void bubbleSort(int[] data) {
+		// TODO Auto-generated method stub
+		boolean flag = true;
+		
+		for(int i=0;i<data.length && flag;i++)
+		{
+			flag = false;
+		
+			for(int j=0;j<data.length-i-1;j++)
+			{
+				if(data[j]>data[j+1])
+				{
+					int temp = data[j];
+					data[j] = data[j+1];
+					data[j+1] = temp;
+					flag = true;
+					
+				}
+			}
+		}
+	
+	}
+
+	private static void out(int[] data) {
+		// TODO Auto-generated method stub
+		for(int i =0;i<data.length;i++)
+		{
+			System.out.print(data[i]+"   ");
+		}
+		System.out.println("");
+		
+	}
+
+}
