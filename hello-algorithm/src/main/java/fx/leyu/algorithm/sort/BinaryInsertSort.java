c917be4fd5b739db1e5b77ab411b81695366846c
fxleyu <fxleyu@qq.com>
2019-08-04 19:45:23 +0800

    [算法] 插入排序 重构

--- hello-algorithm/src/main/java/fx/leyu/algorithm/sort/BinaryInsertSort.java
+++ hello-algorithm/src/main/java/fx/leyu/algorithm/sort/BinaryInsertSort.java
@@ -1,70 +1,39 @@
 package fx.leyu.algorithm.sort;
 
+import java.util.Arrays;
+
 public class BinaryInsertSort {
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
-	  int data[] = {1,2,3,4,5,6,7,8,9,10,1};
-		
-//		int data[] = {10,9,8,7,6,5,4,3,2,1,5,4};
-		
-		binaryInsertSort(data);
-		
-		out(data);
+    public static void main(String[] args) {
+        int data[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 1};
+        binaryInsertSort(data);
+        out(data);
+    }
 
-	}
-	
-	private static void binaryInsertSort(int[] data) {
-		// TODO Auto-generated method stub
-		
-		int low ,mid,high;
-		int i,j;
-		for( i=1;i<data.length;i++)
-		{
-			low = 0;
-			high = i;
-			int temp = data[i];
-			while(low<=high)
-			{
-				mid =(low+high)/2;
-				if(temp>data[mid])
-				{
-					low = mid+1;
-				}
-				else
-				{
-					high = mid-1;
-				}
-			}
-			for( j=i-1;j>high;j--)
-			{
-				data[j+1] = data[j];
-			}
-			data[j+1] = temp;
-		}
-		
-	}
+    private static void binaryInsertSort(int[] data) {
+        for (int i = 1; i < data.length; i++) {
+            int low = 0;
+            int high = i;
+            int temp = data[i];
+            // 找到插入的位置
+            while (low <= high) {
+                int mid = (low + high) / 2;
+                if (temp > data[mid]) {
+                    low = mid + 1;
+                } else {
+                    high = mid - 1;
+                }
+            }
 
-	private static void swap(int data[],int i, int j) {
-		// TODO Auto-generated method stub
-		int temp =  data[i];
-		data[i] = data[j];
-		data[j] = temp;
-	}
+            // 插入位置后的全部右移
+            for (int j = i - 1; j > high; j--) {
+                data[j + 1] = data[j];
+            }
+            data[high + 1] = temp;
+        }
+    }
 
-	private static void out(int[] data) {
-		// TODO Auto-generated method stub
-		for(int i =0;i<data.length;i++)
-		{
-			System.out.print(data[i]+"   ");
-		}
-		System.out.println("");
-		
-	}
-
-
+    private static void out(int[] data) {
+        System.out.println(Arrays.toString(data));
+    }
 }
