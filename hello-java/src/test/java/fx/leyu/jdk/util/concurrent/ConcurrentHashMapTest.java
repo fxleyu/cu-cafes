1dead2b9375094bcdd392842d915df6f43e12040
fxleyu <fxleyu@qq.com>
2019-08-07 00:12:02 +0800

    [验证] ConcurrentHashMap 并发操作时不会 抛 ConcurrentModificationException， related #79  原理待补充

---
+++ hello-java/src/test/java/fx/leyu/jdk/util/concurrent/ConcurrentHashMapTest.java
@@ -0,0 +1,49 @@
+package fx.leyu.jdk.util.concurrent;
+
+import org.junit.Test;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+
+public class ConcurrentHashMapTest {
+    @Test
+    public void testTwoThreadOperate() throws InterruptedException {
+        Map<String, String> map = new ConcurrentHashMap<>();
+        map.put("1", "1");
+        map.put("3", "3");
+        map.put("5", "5");
+
+        CountDownLatch latch = new CountDownLatch(2);
+        new Thread(() -> {
+            System.out.println("for-each start");
+            for(Map.Entry<String, String> entry : map.entrySet()) {
+                System.out.println("visit key is " + entry.getKey());
+                try {
+                    TimeUnit.SECONDS.sleep(4);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            System.out.println("for-each end");
+            latch.countDown();
+        }).start();
+        new Thread(() -> {
+            System.out.println("add/remove start");
+            map.put("2", "2");
+            System.out.println("add key is 2");
+            try {
+                TimeUnit.SECONDS.sleep(6);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            map.remove("5");
+            System.out.println("remove key is 5");
+            System.out.println("add/remove end");
+            latch.countDown();
+        }).start();
+
+        latch.await();
+    }
+}
