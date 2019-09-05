7b4088836e289fcee0ad10e092371c01b645e0c0
fxleyu <fxleyu@qq.com>
2019-09-05 23:35:47 +0800

    [重构][LeetCode] 1116. 打印零与奇偶数(框架)

---
+++ hello-oj/src/main/java/fx/leyu/leetcode/top1101to1150/problem16/IntConsumer.java
@@ -0,0 +1,10 @@
+package fx.leyu.leetcode.top1101to1150.problem16;
+
+import java.util.function.Consumer;
+
+public class IntConsumer implements Consumer {
+    @Override
+    public void accept(Object o) {
+        System.out.print(o);
+    }
+}
---
+++ hello-oj/src/main/java/fx/leyu/leetcode/top1101to1150/problem16/ZeroEvenOdd.java
@@ -0,0 +1,21 @@
+package fx.leyu.leetcode.top1101to1150.problem16;
+
+public class ZeroEvenOdd {
+    private int n;
+
+    public ZeroEvenOdd(int n) {
+        this.n = n;
+    }
+
+    // printNumber.accept(x) outputs "x", where x is an integer.
+    public void zero(IntConsumer printNumber) throws InterruptedException {
+    }
+
+    public void even(IntConsumer printNumber) throws InterruptedException {
+
+    }
+
+    public void odd(IntConsumer printNumber) throws InterruptedException {
+
+    }
+}
