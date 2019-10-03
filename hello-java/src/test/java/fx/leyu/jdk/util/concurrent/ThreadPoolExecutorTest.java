e5651a0da08a1d1219151dda87f9e9c55806d282
fxleyu <fxleyu@qq.com>
2019-10-03 22:21:01 +0800

    [验证] SynchronousQueue 作为线程池阻塞队列

--- hello-java/src/test/java/fx/leyu/jdk/util/concurrent/ThreadPoolExecutorTest.java
+++ hello-java/src/test/java/fx/leyu/jdk/util/concurrent/ThreadPoolExecutorTest.java
@@ -2,6 +2,7 @@ package fx.leyu.jdk.util.concurrent;
 
 import org.junit.Test;
 
+import java.util.concurrent.SynchronousQueue;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 
@@ -11,5 +12,40 @@ public class ThreadPoolExecutorTest {
     public void test() {
         ThreadPoolExecutor executor = new ThreadPoolExecutor(20, 20, 3, TimeUnit.SECONDS, null);
         executor.execute(() -> { });
+    }
+
+    @Test
+    public void testSynchronousQueue() throws InterruptedException {
+
+        final SynchronousQueue<Runnable> workQueue = new SynchronousQueue<>();
+        ThreadPoolExecutor executor = new ThreadPoolExecutor(1, 1000, 1000, TimeUnit.SECONDS, workQueue);
+        executor.execute(() -> {
+            System.out.println("start 0");
+            try {
+                TimeUnit.SECONDS.sleep(1);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println("end 0");
+        });
+        executor.execute(() -> {
+            System.out.println("start 1");
+            try {
+                TimeUnit.SECONDS.sleep(1);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println("end 1");
+        });
+        executor.execute(() -> {
+            System.out.println("start 2");
+            try {
+                TimeUnit.SECONDS.sleep(1);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println("end 2");
+        });
+        TimeUnit.SECONDS.sleep(3);
     }
 }
