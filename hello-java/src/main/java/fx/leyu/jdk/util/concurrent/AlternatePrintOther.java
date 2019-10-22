08565273720b37a8131573c2d8cd75ff62da9ca6
fxleyu <fxleyu@qq.com>
2019-10-22 23:38:09 +0800

    [验证] 处理代码规范(单文件)

--- hello-java/src/main/java/fx/leyu/jdk/util/concurrent/AlternatePrintOther.java
+++ hello-java/src/main/java/fx/leyu/jdk/util/concurrent/AlternatePrintOther.java
@@ -5,52 +5,55 @@ import java.util.concurrent.Semaphore;
 /**
  * 有i，j，k三个线程,，i线程输出i，j线程输出j， k线程输出k，
  * 要求，同时启动三个线程,，按顺序输出ijk，循环10次。
+ *
+ * @author fxleyu
  */
 public class AlternatePrintOther {
-    private static final Semaphore semaphoreI = new Semaphore(1);
-    private static final Semaphore semaphoreJ = new Semaphore(1);
-    private static final Semaphore semaphoreK = new Semaphore(1);
+    private static final Semaphore SEMAPHORE_I = new Semaphore(1);
+    private static final Semaphore SEMAPHORE_J = new Semaphore(1);
+    private static final Semaphore SEMAPHORE_K = new Semaphore(1);
+    private static final int MAX_VALUE = 10;
 
     public static void main(String[] args) throws InterruptedException {
         Thread threadI = new Thread(() -> {
-            for (int i = 0; i < 10; i++) {
+            for (int i = 0; i < MAX_VALUE; i++) {
                 try {
-                    semaphoreI.acquire();
+                    SEMAPHORE_I.acquire();
                     System.out.println("i");
-                    semaphoreJ.release();
+                    SEMAPHORE_J.release();
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
             }
         });
         Thread threadJ = new Thread(() -> {
-            for (int i = 0; i < 10; i++) {
+            for (int i = 0; i < MAX_VALUE; i++) {
                 try {
-                    semaphoreJ.acquire();
+                    SEMAPHORE_J.acquire();
                     System.out.println("j");
-                    semaphoreK.release();
+                    SEMAPHORE_K.release();
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
             }
         });
         Thread threadK = new Thread(() -> {
-            for (int i = 0; i < 10; i++) {
+            for (int i = 0; i < MAX_VALUE; i++) {
                 try {
-                    semaphoreK.acquire();
+                    SEMAPHORE_K.acquire();
                     System.out.println("k");
-                    semaphoreI.release();
+                    SEMAPHORE_I.release();
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
             }
         });
-        semaphoreI.acquire();
-        semaphoreJ.acquire();
-        semaphoreK.acquire();
+        SEMAPHORE_I.acquire();
+        SEMAPHORE_J.acquire();
+        SEMAPHORE_K.acquire();
         threadI.start();
         threadJ.start();
         threadK.start();
-        semaphoreI.release();
+        SEMAPHORE_I.release();
     }
 }
