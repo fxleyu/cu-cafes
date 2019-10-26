abf691c3e751ad08154f1ba667f1f3c782972a8e
fxleyu <fxleyu@qq.com>
2019-10-26 12:33:15 +0800

    [验证] 处理代码规范(单文件)

--- effective-java/src/main/java/fx/leyu/effective/concurrent/GenerateSerialNumberUtils.java
+++ effective-java/src/main/java/fx/leyu/effective/concurrent/GenerateSerialNumberUtils.java
@@ -2,24 +2,22 @@ package fx.leyu.effective.concurrent;
 
 import java.util.concurrent.atomic.AtomicLong;
 
+/**
+ * @author fxleyu
+ */
 public class GenerateSerialNumberUtils {
     private static volatile int nexSerialNumber = 0;
-    private static final AtomicLong nexSerialNumberForAtomic = new AtomicLong();
-    
-    /*
+    private static final AtomicLong NEX_SERIAL_NUMBER_FOR_ATOMIC = new AtomicLong();
+
+    /**
      * 有问题：
      * `++`不是原子操作。故其导致安全性失败（safety failure）
      */
     public static int generateSerialNumber() {
         return nexSerialNumber++;
     }
-    
+
     public static long generateSerialNumberForAtomic() {
-        return nexSerialNumberForAtomic.getAndIncrement();
+        return NEX_SERIAL_NUMBER_FOR_ATOMIC.getAndIncrement();
     }
-
-    public static void main(String[] args) {
-        // TODO Auto-generated method stub
-    }
-
 }
