960ba58700b9f2d6b9df6365a2ba8caaa50fb014
fxleyu <fxleyu@qq.com>
2019-11-30 23:25:05 +0800

    [验证] DecimalFormat 格式化使用

---
+++ hello-java/src/test/java/fx/leyu/jdk/text/DecimalFormatTest.java
@@ -0,0 +1,22 @@
+package fx.leyu.jdk.text;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.math.BigDecimal;
+import java.text.DecimalFormat;
+
+/**
+ * @author fxleyu
+ */
+public class DecimalFormatTest {
+    @Test
+    public void test() {
+        Assert.assertEquals(".0120", new DecimalFormat(".0000").format(new BigDecimal("0.012001")));
+        Assert.assertEquals(".012", new DecimalFormat(".####").format(new BigDecimal("0.01200001")));
+        Assert.assertEquals("0.0120", new DecimalFormat("0.0000").format(new BigDecimal("0.01200001")));
+        Assert.assertEquals("0.012", new DecimalFormat("#.####").format(new BigDecimal("0.01201")));
+        Assert.assertEquals("010.0120", new DecimalFormat("000.0000").format(new BigDecimal("10.01200001")));
+        Assert.assertEquals("10.012", new DecimalFormat("###.####").format(new BigDecimal("10.01200001")));
+    }
+}
