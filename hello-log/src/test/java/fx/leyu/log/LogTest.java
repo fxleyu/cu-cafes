7ad0d8106a3981b557cea3801e7860f0d6bf82cf
fxleyu <fxleyu@qq.com>
2019-12-09 23:48:59 +0800

    [测试] 添加参考

--- hello-log/src/test/java/fx/leyu/log/LogTest.java
+++ hello-log/src/test/java/fx/leyu/log/LogTest.java
@@ -12,6 +12,7 @@ public class LogTest {
 
     @Test
     public void testLog() {
+        // https://www.logicbig.com/tutorials/misc/java-logging/slf4j-with-log4j2.html 参考
         LOGGER.info("hello world");
     }
 }
