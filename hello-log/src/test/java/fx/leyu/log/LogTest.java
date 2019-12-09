6039244de70bdea00ed65c5a5be4cf088d21e915
fxleyu <fxleyu@qq.com>
2019-12-09 23:48:19 +0800

    [测试] 运行成功

--- hello-log/src/test/java/fx/leyu/log/LogTest.java
+++ hello-log/src/test/java/fx/leyu/log/LogTest.java
@@ -1,6 +1,5 @@
 package fx.leyu.log;
 
-import org.junit.BeforeClass;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -9,13 +8,7 @@ import org.slf4j.LoggerFactory;
  * @author fxleyu
  */
 public class LogTest {
-    private static Logger LOGGER;
-
-    @BeforeClass
-    public static void test() {
-        System.setProperty("log4j.configurationFile", "log4j2-test.xml");
-        LOGGER = LoggerFactory.getLogger(LogTest.class);
-    }
+    private static Logger LOGGER = LoggerFactory.getLogger(LogTest.class);
 
     @Test
     public void testLog() {
