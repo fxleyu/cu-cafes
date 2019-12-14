4a5935215fce89e8bf9964cc9c18008d030e9386
fxleyu <fxleyu@qq.com>
2019-12-14 15:55:11 +0800

    添加 Junit 模块

---
+++ hello-junit/src/test/java/fx/leyu/junit4/ParameterizedTest.java
@@ -0,0 +1,12 @@
+package fx.leyu.junit4;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+/**
+ * @author fxleyu
+ */
+@RunWith(value = Parameterized.class)
+public class ParameterizedTest {
+
+}
