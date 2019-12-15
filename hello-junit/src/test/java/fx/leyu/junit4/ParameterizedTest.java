c5048843afb79a6eb2d925d1848863c3325228cd
fxleyu <fxleyu@qq.com>
2019-12-15 18:37:21 +0800

    [Junit] 处理固定参数

--- hello-junit/src/test/java/fx/leyu/junit4/ParameterizedTest.java
+++ hello-junit/src/test/java/fx/leyu/junit4/ParameterizedTest.java
@@ -3,10 +3,16 @@ package fx.leyu.junit4;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 
+import java.util.Collection;
+
 /**
  * @author fxleyu
  */
 @RunWith(value = Parameterized.class)
 public class ParameterizedTest {
 
+    @Parameterized.Parameters
+    public static Collection<Integer> getTestParameters() {
+        return null;
+    }
 }
