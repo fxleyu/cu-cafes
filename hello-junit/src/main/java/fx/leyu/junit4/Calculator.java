a1347802639cd7fe3d6fda5a3306fba278a20818
fxleyu <fxleyu@qq.com>
2019-12-15 22:29:09 +0800

    [Junit] 参数运行示例

---
+++ hello-junit/src/main/java/fx/leyu/junit4/Calculator.java
@@ -0,0 +1,10 @@
+package fx.leyu.junit4;
+
+/**
+ * @author fxleyu
+ */
+public class Calculator {
+    public double add(double valueOne, double valueTwo) {
+        return valueOne + valueTwo;
+    }
+}
--- hello-junit/src/test/java/fx/leyu/junit4/ParameterizedTest.java
+++ hello-junit/src/test/java/fx/leyu/junit4/ParameterizedTest.java
@@ -1,8 +1,11 @@
 package fx.leyu.junit4;
 
+import org.junit.Assert;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 
+import java.util.Arrays;
 import java.util.Collection;
 
 /**
@@ -10,9 +13,28 @@ import java.util.Collection;
  */
 @RunWith(value = Parameterized.class)
 public class ParameterizedTest {
+    private double expected;
+    private double valueOne;
+    private double valueTwo;
+
+    public ParameterizedTest(double expected, double valueOne, double valueTwo) {
+        this.expected = expected;
+        this.valueOne = valueOne;
+        this.valueTwo = valueTwo;
+    }
 
     @Parameterized.Parameters
-    public static Collection<Integer> getTestParameters() {
-        return null;
+    public static Collection<Integer[]> getTestParameters() {
+        return Arrays.asList(new Integer[][]{
+                {2, 1, 1},
+                {3, 2, 1},
+                {4, 3, 1},
+        });
+    }
+
+    @Test
+    public void sum() {
+        Calculator calculator = new Calculator();
+        Assert.assertEquals(expected, calculator.add(valueOne, valueTwo), 0);
     }
 }
