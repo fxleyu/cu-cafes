2c44646a353c55dbc886326d00a949536f719a76
fxleyu <fxleyu@qq.com>
2019-12-19 23:39:23 +0800

    学习 EasyMock 失败（生日快乐，勒宇）

---
+++ hello-junit/src/main/java/fx/leyu/junit4/easymock/HelloServe.java
@@ -0,0 +1,8 @@
+package fx.leyu.junit4.easymock;
+
+/**
+ * @author fxleyu
+ */
+public interface HelloServe {
+    void sayHello();
+}
---
+++ hello-junit/src/test/java/fx/leyu/junit4/easymock/HelloServiceMock.java
@@ -0,0 +1,15 @@
+package fx.leyu.junit4.easymock;
+
+import org.junit.Before;
+
+/**
+ * @author fxleyu
+ */
+public class HelloServiceMock {
+    private HelloServe helloServe;
+
+    @Before
+    public void setUp() {
+        //helloServe = Easy
+    }
+}
