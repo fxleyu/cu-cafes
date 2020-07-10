6dae2fc25e18ce89d111dd328b602da44b3b1601
朱智慧 <fxleyu@qq.com>
2020-07-10 14:37:24 +0800

    [CompleteFuture] 初探

---
+++ hello-java/src/test/java/fx/leyu/jdk/util/concurrent/CompleteFutureTest.java
@@ -0,0 +1,30 @@
+package fx.leyu.jdk.util.concurrent;
+
+import com.google.common.collect.Lists;
+import org.apache.commons.collections4.CollectionUtils;
+import org.apache.commons.collections4.ListUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.junit.Test;
+
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.TimeUnit;
+
+public class CompleteFutureTest {
+    @Test
+    public void test() {
+        CompletableFuture<List<String>>  one = CompletableFuture.supplyAsync(() -> {
+            System.out.println(System.currentTimeMillis() + " future 1 START: " + Thread.currentThread().getName());
+            try {
+                TimeUnit.SECONDS.sleep(3);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println(System.currentTimeMillis() + " future 1 END: " + Thread.currentThread().getName());
+            return Lists.newArrayList("one");
+        });
+
+        System.out.println(System.currentTimeMillis() + " main : " + Thread.currentThread().getName());
+        System.out.println(one.join());
+    }
+}
