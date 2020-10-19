5505dafa43998a6948f0ddcdc8a14a41d4fc9ee5
fxleyu <fxleyu@qq.com>
2020-10-19 11:08:00 +0800

    [集合] 对于 Map 进行默写 key 删除

--- hello-java/src/test/java/fx/leyu/jdk/util/MapTest.java
+++ hello-java/src/test/java/fx/leyu/jdk/util/MapTest.java
@@ -1,10 +1,11 @@
 package fx.leyu.jdk.util;
 
 import com.google.common.collect.Maps;
+import org.apache.commons.collections4.MapUtils;
 import org.junit.Assert;
 import org.junit.Test;
 
-import java.util.Map;
+import java.util.*;
 
 public class MapTest {
 
@@ -14,5 +15,28 @@ public class MapTest {
         String key = "key";
         String value = "value";
         Assert.assertEquals(map.getOrDefault(key, value), value);
+    }
+
+    @Test(expected = ConcurrentModificationException.class)
+    public void test() {
+        Map<String, String> map = new HashMap<>();
+        map.put("1", "1");
+        map.put("2", "2");
+
+        for (Map.Entry<String, String> entry : map.entrySet()) {
+            if (Objects.equals(entry.getKey(), "1")) {
+                map.remove("1");
+            }
+        }
+        Assert.assertEquals(1, MapUtils.size(map));
+    }
+
+    @Test
+    public void testWithoutException() {
+        Map<String, String> map = new HashMap<>();
+        map.put("1", "1");
+        map.put("2", "2");
+        map.entrySet().removeIf(stringStringEntry -> Objects.equals(stringStringEntry.getKey(), "1"));
+        Assert.assertEquals(1, MapUtils.size(map));
     }
 }
