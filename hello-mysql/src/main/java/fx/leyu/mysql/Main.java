69d5be4369660a6273602c8886b2216516946a41
勒宇 <zzh234677@alibaba-inc.com>
2020-11-26 16:23:54 +0800

    [mysql] 测试

--- hello-mysql/src/main/java/fx/leyu/mysql/Main.java
+++ hello-mysql/src/main/java/fx/leyu/mysql/Main.java
@@ -5,39 +5,35 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 
+/**
+ * @author fxleyu
+ */
 public class Main {
     public static void main(String[] args) {
         String URL = "jdbc:mysql://127.0.0.1:3306/sys";
         String USER = "root";
         String PASSWORD = "12345678";
-        // 1.加载驱动程序
         try {
+            // 1.加载驱动程序
             Class.forName("com.mysql.jdbc.Driver");
             // 2.获得数据库链接
             Connection conn = DriverManager.getConnection(URL, USER, PASSWORD);
-            // 3.通过数据库的连接操作数据库，实现增删改查（使用Statement类）
-            String name = "张三";
-            //预编译
-            String sql = "select * from sys_config";
+            // 3.通过数据库的连接操作数据库，实现增删改查（使用Statement类）预编译
+            String sql = "SELECT * FROM sys.sys_config;";
             PreparedStatement statement = conn.prepareStatement(sql);
             ResultSet rs = statement.executeQuery();
-//			String sql="select * from userinfo where UserName='"+name+"'";
-//			Statement statement = conn.createStatement();
-//			ResultSet rs = statement.executeQuery(sql);
             // 4.处理数据库的返回结果(使用ResultSet类)
             while (rs.next()) {
-                //System.out.println(rs。);
+                System.out.println("have data");
+                System.out.println(rs.getString(1));
             }
 
             // 关闭资源
             conn.close();
             rs.close();
             statement.close();
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        } catch (SQLException e) {
+        } catch (ClassNotFoundException | SQLException e) {
             e.printStackTrace();
         }
     }
