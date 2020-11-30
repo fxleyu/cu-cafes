338ca51c33856c168161ff0383be445af390ed1b
勒宇 <zzh234677@alibaba-inc.com>
2020-11-30 13:37:00 +0800

    [mysql] 代码优化

--- hello-mysql/src/main/java/fx/leyu/mysql/Main.java
+++ hello-mysql/src/main/java/fx/leyu/mysql/Main.java
@@ -10,31 +10,30 @@ import java.sql.SQLException;
  * @author fxleyu
  */
 public class Main {
-    public static void main(String[] args) {
-        String URL = "jdbc:mysql://127.0.0.1:3306/sys";
-        String USER = "root";
-        String PASSWORD = "12345678";
-        try {
-            // 1.加载驱动程序
-            Class.forName("com.mysql.jdbc.Driver");
-            // 2.获得数据库链接
-            Connection conn = DriverManager.getConnection(URL, USER, PASSWORD);
+    private static final String JDBC_URL = "jdbc:mysql://127.0.0.1:3306/sys";
+    private static final String JDBC_USER = "root";
+    private static final String JDBC_PASSWORD = "12345678";
+
+    public static void main(String[] args) throws ClassNotFoundException {
+        // 1.加载驱动程序
+        Class.forName("com.mysql.jdbc.Driver");
+
+        // 2.获得数据库链接
+        // 自动关闭资源
+        try (Connection conn = DriverManager.getConnection(JDBC_URL, JDBC_USER, JDBC_PASSWORD)) {
             // 3.通过数据库的连接操作数据库，实现增删改查（使用Statement类）预编译
             String sql = "SELECT * FROM sys.sys_config;";
-            PreparedStatement statement = conn.prepareStatement(sql);
-            ResultSet rs = statement.executeQuery();
-            // 4.处理数据库的返回结果(使用ResultSet类)
-            while (rs.next()) {
-                System.out.println("have data");
-                System.out.println(rs.getString(1));
+            try (PreparedStatement statement = conn.prepareStatement(sql)) {
+                // 4.处理数据库的返回结果(使用ResultSet类)
+                try (ResultSet rs = statement.executeQuery()) {
+                    while (rs.next()) {
+                        System.out.println("have data");
+                        System.out.println(rs.getString(1));
+                    }
+                }
             }
-
-            // 关闭资源
-            conn.close();
-            rs.close();
-            statement.close();
-        } catch (ClassNotFoundException | SQLException e) {
-            e.printStackTrace();
+        } catch (SQLException throwable) {
+            throwable.printStackTrace();
         }
     }
 }
