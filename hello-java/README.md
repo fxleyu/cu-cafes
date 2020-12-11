6d5ae6ebec7536c1321dde38c6eed2cd61441794
勒宇 <zzh234677@alibaba-inc.com>
2020-12-11 19:47:06 +0800

    [JavaSE] 新增 GC 信息

--- hello-java/README.md
+++ hello-java/README.md
@@ -5,3 +5,14 @@
 - 集合
 - 并发
 - 反射等
+
+# JVM
+## 使用 CMS
+指定参数 `-XX:+UseConcMarkSweepGC`, 获取 GC 信息如下：
+```
+CodeCacheManager : [Code Cache]
+Metaspace Manager : [Metaspace, Compressed Class Space]
+ParNew : [Par Eden Space, Par Survivor Space]
+ConcurrentMarkSweep : [Par Eden Space, Par Survivor Space, CMS Old Gen]
+```
+
--- hello-java/src/test/java/fx/leyu/jdk/lang/management/ManagementFactoryTest.java
+++ hello-java/src/test/java/fx/leyu/jdk/lang/management/ManagementFactoryTest.java
@@ -3,6 +3,7 @@ package fx.leyu.jdk.lang.management;
 import org.junit.Test;
 
 import java.lang.management.*;
+import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
@@ -20,9 +21,23 @@ public class ManagementFactoryTest {
     }
 
     @Test
+    public void testCompilationMXBean() throws ClassNotFoundException {
+        CompilationMXBean compilationMXBean = ManagementFactory.getCompilationMXBean();
+        System.out.println(compilationMXBean.getName());
+    }
+
+    @Test
     public void testGarbageCollectorMXBeans() {
         List<GarbageCollectorMXBean> garbageCollectorList = ManagementFactory.getGarbageCollectorMXBeans();
         garbageCollectorList.forEach(garbageCollectorMXBean -> System.out.println(garbageCollectorMXBean.getName()));
+    }
+
+    @Test
+    public void testMemoryManagerMXBeans() {
+        List<MemoryManagerMXBean> memoryManagerMXBeanList = ManagementFactory.getMemoryManagerMXBeans();
+        memoryManagerMXBeanList.forEach(bean -> {
+            System.out.println(bean.getName() + " : " + Arrays.toString(bean.getMemoryPoolNames()));
+        });
     }
 
     @Test
