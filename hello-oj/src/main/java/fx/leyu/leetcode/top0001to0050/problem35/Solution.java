782022d037338f80928b492707aec057de5e61ed
fxleyu <fxleyu@qq.com>
2021-03-20 19:34:44 +0800

    REFACTOR

--- hello-oj/src/main/java/fx/leyu/leetcode/top0001to0050/problem35/Solution.java
+++ hello-oj/src/main/java/fx/leyu/leetcode/top0001to0050/problem35/Solution.java
@@ -2,23 +2,32 @@ package fx.leyu.leetcode.top0001to0050.problem35;
 
 public class Solution {
     public int searchInsert(int[] nums, int target) {
-        if (nums == null || nums.length == 0) {
+        if (nums == null) {
             return 0;
         }
-        int max = nums.length - 1;
-        int min = 0;
-        while (max > min) {
-            int index = (max + min) / 2;
-            int value = nums[index];
-            if (value > target) {
-                max = index - 1;
-            } else if (value < target) {
-                min = index + 1;
-            } else {
-                return index;
-            }
+
+        return findIndex(nums, target, 0, nums.length - 1);
+    }
+
+    private int findIndex(int[] nums, int target, int start, int end) {
+        if (start >= end) {
+            return insertIndex(nums, target, start);
         }
 
-        return nums[min] >= target ? min : min + 1;
+        int mid = (start + end) / 2;
+        if (nums[mid] == target) {
+            return mid;
+        }
+
+        return nums[mid] > target
+                ? findIndex(nums, target, start, mid - 1)
+                : findIndex(nums, target, mid + 1, end);
+    }
+
+    /**
+     * 对于查询失败的位置 i：默认 i - 1 小于目标值；i + 1 大于目标值。如果 i 对应值大于目标值，插入 i；否则，插入 i + 1 位置。
+     */
+    private int insertIndex(int[] nums, int target, int index) {
+        return nums[index] >= target ? index : index + 1;
     }
 }
