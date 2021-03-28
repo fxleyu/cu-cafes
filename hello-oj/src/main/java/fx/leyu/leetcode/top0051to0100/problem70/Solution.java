b907ecdc0b44dd0005885cbd5f1e55ee340d479c
fxleyu <fxleyu@qq.com>
2021-03-28 20:55:18 +0800

    leetcode 新增官方解法

--- hello-oj/src/main/java/fx/leyu/leetcode/top0051to0100/problem70/Solution.java
+++ hello-oj/src/main/java/fx/leyu/leetcode/top0051to0100/problem70/Solution.java
@@ -33,4 +33,15 @@ public class Solution {
      *  ????
      *
      */
+
+    // office
+    public int climbStairs1(int n) {
+        int p = 1, q = 1;
+        for (int i = 2; i <= n; i++) {
+            int temp = q;
+            q += p;
+            p = temp;
+        }
+        return q;
+    }
 }
