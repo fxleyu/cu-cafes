16fe416e478029883f14fd30322446f9c2b230ee
fxleyu <fxleyu@qq.com>
2021-05-12 20:30:22 +0800

    [leetcode]  27 移除元素

--- hello-oj/src/main/java/fx/leyu/leetcode/top0001to0050/problem27/Solution.java
+++ hello-oj/src/main/java/fx/leyu/leetcode/top0001to0050/problem27/Solution.java
@@ -2,15 +2,24 @@ package fx.leyu.leetcode.top0001to0050.problem27;
 
 public class Solution {
     public int removeElement(int[] nums, int val) {
-        if (nums == null) {
+        if (nums == null || nums.length < 1) {
             return 0;
         }
-        int index = 0;
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] != val) {
-                nums[index++] = nums[i];
+
+        int start = 0;
+        int end = nums.length - 1;
+        while (start < end) {
+            while (nums[start] != val && start < end) {
+                start++;
+            }
+            while (nums[end] == val && start < end) {
+                end--;
+            }
+            if (start < end) {
+                nums[start++] = nums[end];
+                nums[end--] = val;
             }
         }
-        return index;
+        return nums[start] == val ? start : start + 1;
     }
 }
