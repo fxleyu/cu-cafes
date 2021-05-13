2312e2f25df1bb9ec79506fcdd4c108f99ee110c
fxleyu <fxleyu@qq.com>
2021-05-13 10:28:09 +0800

    [leetcode]  94. 二叉树的中序遍历 非递归

--- hello-oj/src/main/java/fx/leyu/leetcode/top0051to0100/problem94/Solution.java
+++ hello-oj/src/main/java/fx/leyu/leetcode/top0051to0100/problem94/Solution.java
@@ -2,9 +2,7 @@ package fx.leyu.leetcode.top0051to0100.problem94;
 
 import fx.leyu.leetcode.datastructure.TreeNode;
 
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Objects;
+import java.util.*;
 
 /**
  * @author fxleyu
@@ -24,5 +22,29 @@ public class Solution {
         inorderTraversal(root.left, result);
         result.add(root.val);
         inorderTraversal(root.right, result);
+    }
+
+    public List<Integer> inorderTraversal1(TreeNode root) {
+        List<Integer> result = new LinkedList<>();
+        if (Objects.isNull(root)) {
+            return result;
+        }
+
+        // stack 存储非空元素
+        Deque<TreeNode> stack = new LinkedList<>();
+        addStackLeftNode(stack, root);
+        while (!stack.isEmpty()) {
+            TreeNode visit = stack.pollFirst();
+            result.add(visit.val);
+            addStackLeftNode(stack, visit.right);
+        }
+        return result;
+    }
+
+    private void addStackLeftNode(Deque<TreeNode> stack, TreeNode root) {
+        while (Objects.nonNull(root)) {
+            stack.addFirst(root);
+            root = root.left;
+        }
     }
 }
