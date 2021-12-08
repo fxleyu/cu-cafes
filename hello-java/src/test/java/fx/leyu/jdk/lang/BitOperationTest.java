42df0f8b589e89a52b367238113b2e241200bd79
fxleyu <fxleyu@qq.com>
2021-12-08 20:54:35 +0800

    [测试]  处理二进制操作业务逻辑

--- hello-java/src/test/java/fx/leyu/jdk/lang/BitOperationTest.java
+++ hello-java/src/test/java/fx/leyu/jdk/lang/BitOperationTest.java
@@ -8,10 +8,19 @@ public class BitOperationTest {
     public void testAndOperation() {
         Assert.assertEquals(1, 1 & 1);
         Assert.assertEquals(0, 0 & 1);
+        Assert.assertEquals(0, 0 & 2);
+        Assert.assertEquals(0, 0 & 3);
+        Assert.assertEquals(0, 0 & 4);
+
+        int mode = 3;
+        mode &= ~2;
+        System.out.println(mode);
 
         Assert.assertEquals(0, (0 >> 1) & 1);
         Assert.assertEquals(0, (1 >> 1) & 1);
         Assert.assertEquals(1, (2 >> 1) & 1);
         Assert.assertEquals(1, (3 >> 1) & 1);
     }
+
+
 }
