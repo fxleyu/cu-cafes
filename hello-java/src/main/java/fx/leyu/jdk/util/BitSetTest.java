20e21a783610269c607f7676c3f23e70e18c8628
fxleyu <fxleyu@qq.com>
2021-12-08 17:08:07 +0800

    [测试] 学习 BitSet 逻辑

---
+++ hello-java/src/main/java/fx/leyu/jdk/util/BitSetTest.java
@@ -0,0 +1,21 @@
+package fx.leyu.jdk.util;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.BitSet;
+
+/**
+ * @author fxleyu
+ */
+public class BitSetTest {
+
+    @Test
+    public void test() {
+        BitSet bitSet = new BitSet();
+        Assert.assertTrue(!bitSet.get(1));
+        bitSet.set(2);
+        Assert.assertTrue(!bitSet.get(1));
+        Assert.assertTrue(bitSet.get(2));
+    }
+}
