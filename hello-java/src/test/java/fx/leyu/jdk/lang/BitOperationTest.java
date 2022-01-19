e51483e4c36cf7eedb9e93fb0c757c9c020b8c7f
fxleyu <fxleyu@qq.com>
2022-01-19 22:56:25 +0800

    [测试] 处理位运算

--- hello-java/src/test/java/fx/leyu/jdk/lang/BitOperationTest.java
+++ hello-java/src/test/java/fx/leyu/jdk/lang/BitOperationTest.java
@@ -3,6 +3,9 @@ package fx.leyu.jdk.lang;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.Calendar;
+import java.util.TimeZone;
+
 public class BitOperationTest {
     @Test
     public void testAndOperation() {
@@ -20,6 +23,16 @@ public class BitOperationTest {
         Assert.assertEquals(0, (1 >> 1) & 1);
         Assert.assertEquals(1, (2 >> 1) & 1);
         Assert.assertEquals(1, (3 >> 1) & 1);
+
+        Calendar calendar = Calendar.getInstance(TimeZone.getTimeZone("GMT+8"));
+        System.out.println(calendar);
+        long now=calendar.getTime().getTime();
+
+        calendar.set(Calendar.HOUR_OF_DAY, 24);
+        calendar.set(Calendar.MINUTE, 0);
+        calendar.set(Calendar.SECOND, 0);
+        System.out.println(now);
+        System.out.println(calendar.getTime().getTime() - now);
     }
 
 
