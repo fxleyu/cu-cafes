43cc5c4dcc8c79b22a165b2038848918ada9688a
fxleyu <fxleyu@qq.com>
2022-05-28 22:52:09 +0800

    [leetcode] 34 二分查找 - 新增官方回答
    Signed-off-by: fxleyu <fxleyu@qq.com>

--- hello-oj/src/main/java/fx/leyu/leetcode/top0001to0050/problem34/Solution.java
+++ hello-oj/src/main/java/fx/leyu/leetcode/top0001to0050/problem34/Solution.java
@@ -3,6 +3,31 @@ package fx.leyu.leetcode.top0001to0050.problem34;
 import java.util.Objects;
 
 class Solution {
+    /**
+     * 官方解答
+     */
+    public int[] searchRangeX(int[] nums, int target) {
+        int leftIdx = binarySearch(nums, target, true);
+        int rightIdx = binarySearch(nums, target, false) - 1;
+        if (leftIdx <= rightIdx && rightIdx < nums.length && nums[leftIdx] == target && nums[rightIdx] == target) {
+            return new int[]{leftIdx, rightIdx};
+        }
+        return new int[]{-1, -1};
+    }
+
+    public int binarySearch(int[] nums, int target, boolean lower) {
+        int left = 0, right = nums.length - 1, ans = nums.length;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (nums[mid] > target || (lower && nums[mid] >= target)) {
+                right = mid - 1;
+                ans = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return ans;
+    }
     public int[] searchRange(int[] nums, int target) {
         if (Objects.isNull(nums) || nums.length == 0) {
             return new int[] {-1, -1};
---
+++ hello-oj/src/main/java/fx/leyu/leetcode/top0051to0100/problem69/Solution.java
@@ -0,0 +1,4 @@
+package fx.leyu.leetcode.top0051to0100.problem69;
+
+public class Solution {
+}
