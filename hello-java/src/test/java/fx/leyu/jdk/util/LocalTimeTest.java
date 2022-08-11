6078399d531b65afb172db640bf8c94870eee8c9
fxleyu <fxleyu@qq.com>
2022-08-11 10:34:51 +0800

    [TEST] 处理 LocalDateTime 进行测试

--- hello-java/src/test/java/fx/leyu/jdk/util/LocalTimeTest.java
+++ hello-java/src/test/java/fx/leyu/jdk/util/LocalTimeTest.java
@@ -3,7 +3,10 @@ package fx.leyu.jdk.util;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.time.Instant;
+import java.time.LocalDateTime;
 import java.time.LocalTime;
+import java.util.TimeZone;
 
 public class LocalTimeTest {
     @Test
@@ -13,5 +16,23 @@ public class LocalTimeTest {
         LocalTime after = LocalTime.parse("21:00");
         Assert.assertTrue(before.isBefore(localTime));
         Assert.assertTrue(after.isAfter(localTime));
+    }
+
+    @Test
+    public void test() {
+        System.out.println(getNextDayMsOfXHour(System.currentTimeMillis(), -1));
+    }
+
+    public static Long getNextDayMsOfXHour(long ms, int x) {
+        LocalDateTime localDateTime = LocalDateTime.ofInstant(Instant.ofEpochSecond(ms/1000), TimeZone.getTimeZone("GMT+08:00").toZoneId());
+        int hour = localDateTime.getHour();
+        int min = localDateTime.getMinute();
+        int s = localDateTime.getSecond();
+        return localDateTime.plusHours(24 + x - hour)
+                .minusMinutes(min)
+                .minusSeconds(s)
+                .atZone(TimeZone.getTimeZone("GMT+08:00").toZoneId())
+                .toInstant()
+                .toEpochMilli();
     }
 }
