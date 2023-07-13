667d8641d018938dd8357b9e30edba683b2503ea
朱智慧 <zhihui.zhu@urbanic.com>
2023-07-13 15:01:46 +0800

    [TEST] 新增对象 clone 测试
    Signed-off-by: 朱智慧 <zhihui.zhu@urbanic.com>

---
+++ hello-java/src/test/java/fx/leyu/jdk/lang/CloneTest.java
@@ -0,0 +1,44 @@
+package fx.leyu.jdk.lang;
+
+import org.junit.Test;
+import org.springframework.beans.BeanUtils;
+
+public class CloneTest {
+    @Test
+    public void test() {
+        CloneObj obj = new CloneObj();
+        obj.cloneObjItem = new CloneObjItem();
+        obj.cloneObjItem.item = "HelloWorld";
+
+        CloneObj objCopy = new CloneObj();
+        BeanUtils.copyProperties(obj, objCopy);
+        System.out.println(obj.cloneObjItem);
+        System.out.println(objCopy);
+    }
+
+    public static class CloneObj {
+        private CloneObjItem cloneObjItem;
+
+
+        public CloneObjItem getCloneObjItem() {
+            return cloneObjItem;
+        }
+
+        public void setCloneObjItem(CloneObjItem cloneObjItem) {
+            this.cloneObjItem = cloneObjItem;
+        }
+    }
+
+    public static class CloneObjItem {
+        private String item = "";
+
+
+        public String getItem() {
+            return item;
+        }
+
+        public void setItem(String item) {
+            this.item = item;
+        }
+    }
+}
