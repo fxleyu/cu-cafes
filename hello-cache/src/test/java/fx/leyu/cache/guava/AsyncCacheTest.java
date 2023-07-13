247727bde513b41114a18afe23ff612e93383b42
fxleyu <fxleyu@qq.com>
2023-07-13 15:01:26 +0800

    [TEST] 处理缓存业务逻辑
    Signed-off-by: 朱智慧 <zhihui.zhu@urbanic.com>

--- hello-cache/src/test/java/fx/leyu/cache/guava/AsyncCacheTest.java
+++ hello-cache/src/test/java/fx/leyu/cache/guava/AsyncCacheTest.java
@@ -17,14 +17,14 @@ import java.util.concurrent.*;
 public class AsyncCacheTest {
     @Test
     public void test() throws ExecutionException, InterruptedException {
-        LoadingCache<String, String> loadingCache = CacheBuilder.newBuilder()
+        final LoadingCache<String, String> loadingCache = CacheBuilder.newBuilder()
                 //设置并发级别为1，并发级别是指可以同时写缓存的线程数
                 .concurrencyLevel(1)
                 //设置缓存容器的初始容量为10
                 .initialCapacity(3)
                 //设置缓存最大容量为10，超过10之后就会按照LRU最近虽少使用算法来移除缓存项
                 .maximumSize(3)
-                .refreshAfterWrite(1, TimeUnit.SECONDS)
+                .refreshAfterWrite(10, TimeUnit.SECONDS)
                 .removalListener(notification -> {
                     try {
                         System.out.println(notification.getKey() + " " + notification.getValue() + " remove by " + notification.getCause());
@@ -41,13 +41,14 @@ public class AsyncCacheTest {
                     @ParametersAreNonnullByDefault
                     public String load(String key) throws Exception {
                         System.out.println("load " + key);
+                        TimeUnit.SECONDS.sleep(1);
                         if (Objects.equals(key, "1")) {
                             return "null"  + System.currentTimeMillis();
                         }
                         return key +"'s value is missing" + System.currentTimeMillis();
                     }
 
-                    public ListenableFuture<String> reload(String key, String oldValue) throws Exception {
+                    /*public ListenableFuture<String> reload(String key, String oldValue) throws Exception {
                         System.out.println();
                         if (Objects.equals(key, "1")) {
                             throw new RuntimeException();
@@ -61,13 +62,46 @@ public class AsyncCacheTest {
                                 , 0
                                 , TimeUnit.SECONDS
                                 , new LinkedBlockingQueue<>(1000)));
-                    }
+                    }*/
                 });
+        new Thread(() -> {
+            try {
+                String value = loadingCache.get("key");
+                System.out.println("THREAD value " + value);
+            } catch (ExecutionException e) {
+                throw new RuntimeException(e);
+            }
+        }).start();
+        new Thread(() -> {
+            try {
+                String value = loadingCache.get("key");
+                System.out.println("THREAD value " + value);
+            } catch (ExecutionException e) {
+                throw new RuntimeException(e);
+            }
+        }).start();
+        new Thread(() -> {
+            try {
+                String value = loadingCache.get("key");
+                System.out.println("THREAD value " + value);
+            } catch (ExecutionException e) {
+                throw new RuntimeException(e);
+            }
+        }).start();
+        new Thread(() -> {
+            try {
+                String value = loadingCache.get("key");
+                System.out.println("THREAD value " + value);
+            } catch (ExecutionException e) {
+                throw new RuntimeException(e);
+            }
+        }).start();
+        System.out.println("start END ");
 
         String value = loadingCache.get("key");
-        value = loadingCache.get("1");
+        value = loadingCache.getIfPresent("1");
         TimeUnit.SECONDS.sleep(2);
-        value = loadingCache.get("key");
+        value = loadingCache.getIfPresent("key");
         value = loadingCache.get("1");
         value = loadingCache.get("2");
         value = loadingCache.get("3");
