4d1f896415e1e4d52a931a33b4512057ff8ad0db
fxleyu <fxleyu@qq.com>
2023-10-08 19:33:49 +0800

    [TEST] 测试 stream NPE 的场景
    Signed-off-by: 朱智慧 <zhihui.zhu@urbanic.com>

--- hello-java/src/test/java/fx/leyu/jdk/util/ListTest.java
+++ hello-java/src/test/java/fx/leyu/jdk/util/ListTest.java
@@ -1,16 +1,32 @@
 package fx.leyu.jdk.util;
 
+import com.google.common.collect.Lists;
+import org.apache.commons.collections4.ListUtils;
+import org.junit.Assert;
 import org.junit.Test;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 public class ListTest {
     @Test
     public void testEmpty4ForEach() {
-        List<Integer> list = Collections.emptyList();
-        for (Integer integer : list) {
-            System.out.println(integer);
+        List<String> list = Collections.emptyList();
+        for (String str : list) {
+            System.out.println(str);
         }
+        Assert.assertNotNull(list);
+    }
+
+    @Test
+    public void testString() {
+        List<String> strList = Lists.newArrayList("A", "B", null, null, "C", "D");
+        List<Integer> sizeList = strList.stream()
+                .filter(Objects::nonNull)
+                .map(String::length)
+                .collect(Collectors.toList());
+        Assert.assertEquals(4, sizeList.size());
     }
 }
